36638ccf2f115a952bebc2d03eb5f04dcc63d114
Ed <ed321@hotmail.co.uk>
2022-01-13 13:45:09 +0000

    Ability to organise users into groups

--- directory.rb
+++ directory.rb
@@ -1,15 +1,49 @@
 def input_students
+  students = []
+
   puts "Please enter the names of new students"
   puts "To finish, press return twice"
-  students = []
+
   name = gets.chomp
+  months = [
+      "January", "February", "March", "April", "May",
+      "June", "July", "August", "September", "October",
+      "November", "December"
+    ]
+
   # waits until name variable is empty to trigger double return exit
+  counter = 0
   while !name.empty? do
-    students << {name: name, cohort: :november, dob: "value", height: "value", hobbies: "value"}
+    students << {name: name, dob: "value", height: "value", hobbies: "value"}
     puts "Now we have #{students.count} students"
+    puts "Assign student to a cohort (If empty will be assigned to the current cohort)"
+
+    # Handle Assignment of cohort
+    cohort = gets.chomp
+    while true do
+      if cohort.empty?
+        cohort = months[0] # current month
+        puts "Assigned to #{months[0]} cohort."
+        students[counter][:cohort] = cohort.to_sym
+        break
+      else
+        cohort.capitalize!
+        if months.include?(cohort)
+          puts "Assigned to #{cohort} cohort."
+          students[counter][:cohort] = cohort.to_sym
+          break
+        else
+          puts "Try again - not a valid Cohort"
+          cohort = gets.chomp
+        end
+      end
+    end
+    puts students[counter]
+    # count the students to avoid additional loops
+    counter += 1
     name = gets.chomp
-    
   end
+
   students
 end
 
@@ -28,6 +62,7 @@ def print(students)
       puts "    DOB: " + "#{student[:dob]}".center(12)
       puts " Height: " + "#{student[:height]}".center(12)
       puts "Hobbies: " + "#{student[:hobbies]}".center(12)
+      puts "------------------------------------"
     end
     idx +=1
   end
