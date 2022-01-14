f74def584bce2ca694c30fd1d177fdaf40b7e1e3
Ed <ed321@hotmail.co.uk>
2022-01-14 10:43:07 +0000

    Created interactive menu

--- directory.rb
+++ directory.rb
@@ -85,9 +85,32 @@ def print_footer(students)
     plural = "Overall, we have #{students.count} great student"
     puts students.count == 1 ? plural : (plural + "s")
   end
+
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+
+  end
+
+end
+
+interactive_menu
