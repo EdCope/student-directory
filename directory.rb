e59e5e98a693aba626479cc21deba710b8c3921a
Ed <ed321@hotmail.co.uk>
2022-01-14 11:37:18 +0000

    Added saving to students.csv

--- directory.rb
+++ directory.rb
@@ -93,12 +93,25 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort],
+     student[:dob], student[:height], student[:hobbies]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -109,6 +122,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
